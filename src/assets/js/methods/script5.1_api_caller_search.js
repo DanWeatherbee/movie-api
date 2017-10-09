3ddb00b0f0b07bd1c7a64575861ea200ec9bff37
Dan Weatherbee <dan@jedi-clan.us>
2017-10-09 06:57:16 -0700

    feat: added enter keypress for input.

--- src/assets/js/methods/script5.1_api_caller_search.js
+++ src/assets/js/methods/script5.1_api_caller_search.js
@@ -1,19 +1,25 @@
 API.prototype.callSearch = async function(
     response,
     responseArray,
-    callType
+    callType,
+    searchInput
 ) {
     'use strict';
+    var self = this;
     $('#error-msg').hide();
     $('.content').html('');
     var StartTimeMs = Date.now();
     console.log("Start Execution Time Caller Method - " + StartTimeMs + " Milliseconds");
-    var txt = $("#fda-search").val();
+    self.searchInput = $("#fda-search");
+    var txt = self.searchInput.val();
     console.log(txt);
-    var caller;
-    // Determine call type. e.g. caller = API_CALL_MOVIES_THEATERS
-    // code here
 
+    var caller;
+    /*
+    Determine call type. e.g. caller = API_CALL_MOVIES_THEATERS
+    Instantiate new object.
+    code goes here;
+    */
     var API_FOOD_SEARCH = new API(
         "https://api.fda.gov/",
         "food/event.json?",
@@ -46,3 +52,11 @@ API.prototype.callSearch = async function(
     var TotalTime = EndTimeMs - StartTimeMs;
     console.log("Total Execution Time Caller Method - " + TotalTime + " Milliseconds");
 };
+
+
+// Allow enter key vrs btn to call the API.
+$("#fda-search").on("keypress", function(e) {
+    if (e.which == 13) {
+        $("#go").click();
+    }
+});
